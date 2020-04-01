674fe86ddccd7dc029fc126abba55c30b86ced9f
Giulai <giu.delci@gmail.com>
2020-04-02 00:02:56 +0200

    ok broker bridge and riot

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
-# IoT-Assignment1
-
-HOW TO RUN THE SYSTEM: 
-Requirement:
-		-python3 (to install: sudo apt-get install python3.6)
-		-mqtt library (to install: pip3 install paho-mqtt)
-
-Open two different terminal into the downloaded folder and run using "python3 station1.py" and "python3 station2.py".
-Now open the data_station.html
-
-
-Blog Post: https://www.linkedin.com/pulse/virtual-environmental-station-giulia-del-citto
-
-Video: https://www.youtube.com/watch?v=_BEFHXWqMUI
+RUNNING:
+	1. local broker: open a terminal into mosquitto.rsmb/rsmb/src and execute ./broker_mqtts config.conf
+	2. RIOT : follow the instruction in the readme.md in the RIOT folder
+	3. bridge: python3 bridge.py
+					 
---
+++ virtual_station1/station2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import paho.mqtt.client as mqtt
+import time
+import random
+import numpy as np
+import json
+
+def data_generator():
+	data=[0,0,0,0,0]
+	#temperature
+	data[0]=round(random.uniform(-50, 50),1)
+	#humidity
+	data[1]=round(random.uniform(0, 100),1)
+	#wind direction
+	data[2]=random.randint(0, 360)
+	#wind intensity
+	data[3]=round(random.uniform(0, 100),1)
+	#rain height
+	data[4]=round(random.uniform(0, 50),1)
+	
+	return data
+	
+#check the connection 
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        client.connected_flag=True #set flag
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=",rc)
+
+def main(): 
+	#Broker value 127.0.0.1 if in local, demo.thingsboard.io in livedemo version. Port 1883 for the MQTT connection
+	mqtt.Client.connected_flag=False
+	broker="demo.thingsboard.io"
+	port=1883
+	#topic of the publish is the telemetry of the device (Environment_station2). The id is the device access token
+	topic="v1/devices/me/telemetry"
+	id_client="0P4MFpOyGlXQKlusWN14"
+	passwrd=""
+	#one key for each sensor
+	key=["temperature", "humidity", "wind_direction", "wind_intensity", "rain_height"]
+	
+	if id_client !="" :
+		client = mqtt.Client()
+		client.username_pw_set(id_client, passwrd)
+	
+	#connection and check
+	client.connect(broker, port)
+	client.on_connect=on_connect
+	
+	while not client.connected_flag :
+		client.loop()
+		time.sleep(2)
+		
+	#loop for generation and publishing
+	while(1):
+		data=data_generator()
+		print(data)
+		data_out=json.dumps({key[0]:data[0],key[1]:data[1],key[2]:data[2],key[3]:data[3],key[4]:data[4]})
+		client.publish(topic, data_out,0);
+		time.sleep(2)
+	
+	client.disconnect();
+
+if __name__ == '__main__' : main()
